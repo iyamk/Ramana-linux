fb36143baa4b77165b661b64cde400e74bf4b51b
iyamk <105396562+iyamk@users.noreply.github.com>
2023-02-03 07:10:17 +0300

    Added some shit for qemu emulation

---
+++ qemu_test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! -f "qemu_image.raw" ]; then
+    qemu-img create -f raw qemu_image.raw 10G
+fi
+
+iso_file=$(ls out/|tail -1)
+qemu-system-x86_64 -cdrom out/$iso_file -m 1G qemu_image.raw
